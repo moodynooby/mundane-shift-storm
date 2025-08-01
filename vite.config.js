2d289464b6c0ac55a26da44eaec8ee1cc5944985
Manas Doshi <98732831+moodynooby@users.noreply.github.com>
2025-08-02 02:07:08 +0530

    0.1.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile";
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,     tailwindcss()],
+  plugins: [viteSingleFile(), react(), tailwindcss()],
 })
